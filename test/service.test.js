256f09a39b3bce12cdf25624ff28364653871475
Gisela Zaimi <http://github.com/giselaz>
2026-07-30 10:55:21 +0000

    fixed unit tests

--- test/service.test.js
+++ test/service.test.js
@@ -13,7 +13,7 @@ describe('ServiceDeskService', () => {
     const { data } = await GET(`${serviceUrl}/Operations`, { auth: { username: 'erin', password: '' } });
     expect(data.value.length).toBe(2);
   });
-    $operationId = 'd0000000-0000-0000-0000-000000000001';
+    $operationId = 'e36977b4-8ef2-46ba-865a-35a125a91b5b';
     it(`creates a Comment on ${$operationId} Operation`, async() => {
         const { data } = await POST(`${serviceUrl}/Operations(ID=${$operationId},IsActiveEntity=true)/ServiceDeskService.addComment`, 
           { description: 'Test comment' }, 
@@ -22,7 +22,7 @@ describe('ServiceDeskService', () => {
         })
     it('blocks unauthorized requester from accessing other Operations', async () => {
         await expect(
-            GET(`${serviceUrl}/Operations(ID=d0000000-0000-0000-0000-000000000003,IsActiveEntity=true)`,
+            GET(`${serviceUrl}/Operations(ID=0c2cea12-7764-4c7f-94d9-9bcc16ebd366,IsActiveEntity=true)`,
                 { auth: { username: 'erin', password: '' } })
           ).rejects.toThrow(/404/);
       });
